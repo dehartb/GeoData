0f73079f29a39d711978a837b585b7abe7012da0
TenYardsAtATime <phase227@gmail.com>
2019-06-23 17:28:46 -0400

    data v2

---
+++ data/BLS data/bls_tblSeries.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE  IF NOT EXISTS `bls` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `bls`;
+-- MySQL dump 10.13  Distrib 5.6.17, for osx10.6 (i386)
+--
+-- Host: localhost    Database: bls
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tblSeries`
+--
+
+DROP TABLE IF EXISTS `tblSeries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblSeries` (
+  `SeriesId` varchar(20) NOT NULL,
+  `AreaCode` varchar(15) DEFAULT NULL,
+  `ItemCode` varchar(15) DEFAULT NULL,
+  `SeriesTitle` varchar(500) DEFAULT NULL,
+  `FootnoteCodes` varchar(15) DEFAULT NULL,
+  `BeginYear` varchar(5) DEFAULT NULL,
+  `BeginPeriod` varchar(5) DEFAULT NULL,
+  `EndYear` varchar(5) DEFAULT NULL,
+  `EndPeriod` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`SeriesId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-28 21:41:10
